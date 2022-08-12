f3b088db69335970a08e3f732848db1e12e030cc
Nadya Baranova <12nadya11@gmail.com>
2022-08-12 20:51:20 +0300

    add: finish tast Stack

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.array = [];
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	push(element) {
+		this.array.push(element);
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	pop() {
+		this.num = this.array.pop();
+		return this.num;
+
+	}
+
+	peek() {
+		return this.array[this.array.length - 1];
+	}
 }
 
 module.exports = {
-  Stack
+	Stack
 };
